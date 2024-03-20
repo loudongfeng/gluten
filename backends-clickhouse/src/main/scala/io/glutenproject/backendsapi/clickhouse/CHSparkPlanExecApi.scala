d98c4abb9e9d9c3105c703d2f4ffd23b18ca7049
nemonlou <nemonlou@qq.com>
2024-03-20 07:21:49 +0530

    [GLUTEN-4946][CH] Fix avg(bigint) overflow

--- backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHSparkPlanExecApi.scala
+++ backends-clickhouse/src/main/scala/io/glutenproject/backendsapi/clickhouse/CHSparkPlanExecApi.scala
@@ -36,6 +36,7 @@ import org.apache.spark.serializer.Serializer
 import org.apache.spark.shuffle.{GenShuffleWriterParameters, GlutenShuffleWriterWrapper, HashPartitioningWrapper}
 import org.apache.spark.shuffle.utils.CHShuffleUtil
 import org.apache.spark.sql.{SparkSession, Strategy}
+import org.apache.spark.sql.catalyst.CHAggregateFunctionRewriteRule
 import org.apache.spark.sql.catalyst.catalog.BucketSpec
 import org.apache.spark.sql.catalyst.catalog.CatalogTypes.TablePartitionSpec
 import org.apache.spark.sql.catalyst.expressions._
@@ -511,6 +512,7 @@ class CHSparkPlanExecApi extends SparkPlanExecApi {
   override def genExtendedOptimizers(): List[SparkSession => Rule[LogicalPlan]] = {
     List(
       spark => new CommonSubexpressionEliminateRule(spark, spark.sessionState.conf),
+      spark => CHAggregateFunctionRewriteRule(spark),
       _ => CountDistinctWithoutExpand
     )
   }
